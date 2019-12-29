d5f52712609e64e309da24edc385d50fa80f7085
Kring Villanueva <ksvillanueva7@gmail.com>
2019-12-29 01:58:37 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,11 +1,13 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pry'
 
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
   #
+  binding.pry
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
   }
